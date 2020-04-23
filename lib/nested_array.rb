0c31d128608394f3514a7b347fabe6888f0d8cb6
Matthew Klein <mtt.l.kln@gmail.com>
2020-04-23 18:02:53 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -54,7 +54,7 @@ def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
 
-matrix[row][column] => new_value
+matrix[row][column] = new_value
 return matrix
 
 end
